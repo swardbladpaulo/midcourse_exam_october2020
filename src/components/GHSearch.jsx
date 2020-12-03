33d7ab21b0c3b9917231592efcf69e0556ac5fae
swardbladpaulo <paulo@swardblads.se>
2020-12-03 12:13:17 +0100

    struggle with semantic stuff, could not put in the set.state function somewhere

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Input } from 'semantic-ui-react';
 import axios from 'axios';
 
-const GHSearch = () => {
-  
-  async GHSearch() {
-    let response = await.axios.
-  }
-  return (
-    <>
-      <Input type="text" name="search" placeholder="Input GH username"/>
-      <Button name="search">Search</Button>
-    </>
-  )
+class GHSearch extends Component {
+  state = {
+    search: "",
+  };
+
+  async search() {
+    let response = await axios.get("https://api.github.com/search/users?");
+    this.setState({ search: response.data });
+  };
+
+  componentDidMount() {
+    this.search();
+  };
+
+  render() {
+    
+  <>
+    <Input type="text" name="search" placeholder="Input GH username"/>
+    <Button name="search">Search</Button>
+    return{ this.setState.search.search}
+  </>
+  };
 }
 
-export default GHSearch
+export default GHSearch;
--- writtenExam.md
+++ writtenExam.md
@@ -3,3 +3,16 @@
 How and when to use `.forEach`, `.map`, `.filter`, and `.find` in JavaScript?
 
 Please give an as extensive explanation as possible with code examples and potential use cases. Write your answer below and use markdown to format your text.
+
+## .map
+The map function let you create a new array that you can break down with new information values from the calling function.
+For example, if you have productdata_table, you can get information from each column that you are calling back from, example item_nr, item_name, item_quantity, item_stock etc.
+
+## .forEach
+The forEach function calls a function once before to do something else.
+
+## .filter
+
+## .find
+
+
